2a0eac6f8f4018ed4bd5526addadfb4d329983a6
Seeeerzh <80677176+Seeeerzh@users.noreply.github.com>
2022-12-19 22:57:59 +0200

    Update Program.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -17,11 +17,10 @@ builder.Services.AddScoped<TechService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
